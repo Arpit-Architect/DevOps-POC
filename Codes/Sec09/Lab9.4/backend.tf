248069b8fb6cad261400c94f688bdb220c4503f0
Arpit Saxena <arpit.saxena@tietoevry.com>
2021-06-15 23:16:49 +0530

    updated code with rgstorageaccount

--- Codes/Sec09/Lab9.4/backend.tf
+++ Codes/Sec09/Lab9.4/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend azurerm {
       resource_group_name  = "Terra-rg"
-      storage_account_name = "remotesa01"
+      storage_account_name = "remotesatest0010"
       container_name       = "tfstate"
-      key                  = "Lab9.4.tfstate"
+      key                  = "testdeployinfra.tfstate"
   }
 }
